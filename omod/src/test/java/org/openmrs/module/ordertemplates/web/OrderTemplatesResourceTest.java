1328dc3f7c6d78238b2d4a89abcfff9713cf1d1c
Samuel Male <samuelsmalek@gmail.com>
2022-08-08 10:25:28 +0300

    Add length attribute to pass field length validation (#3)

--- omod/src/test/java/org/openmrs/module/ordertemplates/web/OrderTemplatesResourceTest.java
+++ omod/src/test/java/org/openmrs/module/ordertemplates/web/OrderTemplatesResourceTest.java
@@ -35,7 +35,7 @@ public class OrderTemplatesResourceTest extends BaseModuleWebContextSensitiveTes
 	public void testCreateOrderTemplate() throws IOException {
 		// setup
 		String json = "{" + "\"name\": \"Abacavir/dolutegravir/lamivudine template\","
-				+ "\"uuid\": \"35f83cd4-64ev-4en7-a5f3-364d3b14a63m\","
+		        + "\"uuid\": \"35f83cd4-64ev-4en7-a5f3-364d3b14a63m\","
 		        + "\"concept\": \"65f83cd4-64e3-4en7-a5f3-364d3b14a634\","
 		        + "\"drug\": \"pe2323fa-6fa0-4618-fb59-6765997d844m\"," + "\"template\": {"
 		        + "\"dosingType\": \"org.openmrs.SimpleDosingInstructions\"," + "\"instructions\": {" + "\"dose\": 1,"
@@ -54,7 +54,7 @@ public class OrderTemplatesResourceTest extends BaseModuleWebContextSensitiveTes
 		assertThat(wasCreated.getId(), notNullValue());
 		assertThat(wasCreated.getName(), is("Abacavir/dolutegravir/lamivudine template"));
 	}
-
+	
 	@Test
 	public void testRetrieveOne() {
 		OrderTemplate existing = resource.getByUniqueId("01b8f6b7-dc0e-4346-b818-f3e9cd24dfdb");
